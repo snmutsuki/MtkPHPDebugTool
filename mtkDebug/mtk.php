4c3eda832bcc52d2dc35f86490e5fc4d691f8ec5
mutsuki <shiinamutsuki@vip.qq.com>
2019-11-16 16:20:44 +0800

    replace mtk.php

--- mtkDebug/mtk.php
+++ mtkDebug/mtk.php
@@ -2,13 +2,15 @@
 namespace mtk;
 
 define("ROOT_DIR",realpath(__DIR__)."/");
+define("CORE_DIR",ROOT_DIR."mtk/");
+define("APP_DIR",ROOT_DIR."app/");
 define("WEBSITE_NAME","/mtkDebug/");
 
 require ROOT_DIR.'/mtk/Loader.php';
 
 Loader::register();
 
-\mtk\App::init()->show();
+\mtk\App::getInstance()->start();
 
 
 
